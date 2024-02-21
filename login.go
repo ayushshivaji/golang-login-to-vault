d3706a120e9799d87a8b5387f76368299f3d433d
Ayush <>
2024-02-22 02:08:02 +0530

    Add login functionality

--- login.go
+++ login.go
@@ -1,52 +1,82 @@
 // +build darwin
-// +build linux 
 
 package main
 
 import (
 	"fmt"
-	// "log"
-	"os/exec"
+	"log"
+	"os"
+    "os/exec"
 )
 
-// if vault not found in path then downlaod vault binary and extract it 
-
-// const VAULT_ADDR="https://lookup.vimaan.app"
+const VAULT_ADDR="https://lookup.vimaan.app"
 const SSH_KEY_LOCATION="$HOME/.ssh/id_rsa"
-const VAULT_DOWNLOAD_LINK="https://releases.hashicorp.com/vault/1.15.5/vault_1.15.5_linux_arm64.zip"
-
+const VAULT_DOWNLOAD_LINK="https://releases.hashicorp.com/vault/1.15.5/vault_1.15.5_darwin_arm64.zip"
+const SSH_SIGNING_ENGINE="ssh-test-blrvm2"
 // download package using go
 // unzip using go
 func main(){
+    vaultNeedsToBeDownloaded := check_if_vault_exists()
+    if vaultNeedsToBeDownloaded == -1{
+        downloadVault()
+        unzipVault()
+    }
     vaultLogin()
 }
 
-func vaultLogin() {
-    command_to_check_if_vault_exists := "vault"
+func unzipVault(){
+    command_to_unzip := "unzip vault.zip"
+    cmd_unzip := exec.Command("sh", "-c", command_to_unzip)
+    output_unzip, err_unzip := cmd_unzip.CombinedOutput()
+    if err_unzip != nil {
+        log.Fatalf("vault unzipping failed with %s\n", err_unzip)
+    }
+    fmt.Printf("unzip successfull.", string(output_unzip))
+}
+
+func downloadVault(){
+    command_to_download := "curl "+VAULT_DOWNLOAD_LINK+" -o vault.zip"
+	cmd_download := exec.Command( "sh", "-c", command_to_download )
+    output_download, err_download := cmd_download.CombinedOutput()
+	if err_download != nil {
+		log.Fatalf("vault login failed with %s\n", err_download)
+	}
+    fmt.Printf("vault download successful. Output:\n%s\n", string(output_download))
+}
+
+func check_if_vault_exists()(int){
+    path := os.Getenv("PATH")
+    cwd := os.Getenv("PWD")
+    newPath := path + ":" + cwd
+    os.Setenv("PATH", newPath)
+    // fmt.Printf("new path: %s", os.Getenv("PATH"))
+
+    command_to_check_if_vault_exists := "which vault"
     cmd_check := exec.Command("sh", "-c", command_to_check_if_vault_exists)
     output_check, err_check := cmd_check.CombinedOutput()
     if err_check != nil {
-        // downloadVault()
-        fmt.Println("vault not found in path. Downloading vault")
+        fmt.Println("vault not found in path. Will Download vault")
+        return -1
     }
     fmt.Printf("vault found in path. Output:\n%s\n", string(output_check))
+    return 1
+}
 
-    // command_to_download := "wget "+VAULT_DOWNLOAD_LINK+" -O vault.zip"
-	// cmd_download := exec.Command( "sh", "-c", command_to_download )
 
-    // command_to_unzip := "unzip vault.zip"
-    // cmd_unzip := exec.Command("sh", "-c", command_to_unzip)
-	
-    // output_download, err_download := cmd_download.CombinedOutput()
-    // output_unzip, err_unzip := cmd_unzip.CombinedOutput()
+func vaultLogin() {
+    loginCommandString := "vault login -method=oidc"
+    loginCommand := exec.Command("sh", "-c", loginCommandString)
+    // fmt.Printf(os.Getenv("PATH"))
+    os.Setenv("VAULT_ADDR", VAULT_ADDR)
+    outputLogin, errLogin := loginCommand.CombinedOutput()
+    fmt.Printf("%s",loginCommand)
+    fmt.Printf("%s",string(outputLogin))
+    if errLogin != nil {
+        log.Fatalf("vault login failed with %s\n", errLogin)
+    }
+    fmt.Printf("vault login successful. Output:\n%s\n", string(outputLogin))
+}
 
-	// if err_download != nil {
-	// 	log.Fatalf("vault login failed with %s\n", err_download)
-	// }
-    // if err_unzip != nil {
-    //     log.Fatalf("vault login failed with %s\n", err_unzip)
-    // }
-
-    // fmt.Printf("vault download successful. Output:\n%s\n", string(output_download))
-    // fmt.Printf("unzip successfull.", string(output_unzip))
+func signSshKey(){
+    signCommandString := "vault write -field=signed_key ssh-client-signer/sign/my-role public_key=@$HOME/.ssh/id_rsa.pub > id_rsa-cert.pub"
 }
